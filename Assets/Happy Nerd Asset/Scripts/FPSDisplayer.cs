71b8fdef0c6195e78297256fa543865c59bcf8df
StevenLiu1015 <sliu11124@gmail.com>
2022-11-19 15:09:20 +0800

    更新Icon及Gameplay場景

---
+++ Assets/Happy Nerd Asset/Scripts/FPSDisplayer.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class FPSDisplayer : MonoBehaviour
+{
+    [SerializeField]
+    private Text fpsText = null;
+
+    [SerializeField]
+    private float updateFrequency = .3f;
+
+    float fps = 0f;
+
+    private void Start()
+    {
+        fpsText = GetComponent<Text>();
+
+        InvokeRepeating(nameof(DisplayFPS), 0f, updateFrequency);
+    }
+
+    private void Update()
+    {
+        fps = (1f / Time.deltaTime);
+    }
+
+    private void DisplayFPS()
+    {
+        fpsText.text = fps.ToString("0") + " FPS";
+    }
+}
